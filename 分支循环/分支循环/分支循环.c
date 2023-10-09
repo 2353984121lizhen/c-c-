804c3c1eff04b020a2c99d9d3a2db9cb79c6c47f
2353984121lizhen <146803208+2353984121lizhen@users.noreply.github.com>
2023-10-09 18:30:20 +0800

    Add files via upload

---
+++ 分支循环/分支循环/分支循环.c
@@ -0,0 +1,125 @@
+#include <stdio.h>
+#include <string.h>
+#include <Windows.h>
+#include <stdlib.h>
+int main() {
+
+
+
+
+
+	//int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
+	//int left = 0;
+	//int sz = sizeof(arr) / sizeof(arr[0]);
+	//int right = sz - 1;
+	//int k = 7;
+	//while (left <= right) {
+	//	int mid = (right + left) / 2;
+	//	if (arr[mid] > k) {
+	//		right = mid - 1;
+	//	}
+	//	else if (arr[mid] < k) {
+	//		left = mid + 1;
+	//	}
+	//	else {
+	//		printf("找到了,下标为%d", mid);
+	//		break;
+	//	}
+
+	//}
+	//if (left > right) {
+	//	printf("找不到");
+	//}
+
+
+
+
+
+
+
+
+
+
+//	char arr1[] = "welcome  to  bit !!!!!!";
+//	char arr2[] = "#######################";
+//	int left = 0;
+//	int right = strlen(arr1) - 1;
+//	while (left <= right) {
+//	arr2[right] = arr1[right];
+//	arr2[left] = arr1[left];
+//	printf("%s\n", arr2);
+//	
+//	Sleep(1000);
+//	system("cls");
+//	left++;
+//	right--;
+//}
+//	printf("%s\n", arr2);
+
+
+
+
+
+	//int i = 0;
+	//char password[20] = {0};
+	//for ( i = 0; i < 3; i++) {
+	//	printf("请输入密码->");
+	//	scanf_s("%s",password,20);
+	//	
+	//	char c[20] = {"123456"};
+	//	if (strcmp(password,c) == 0)//strcmp(对比的字符串,对比的字符串)   返回值为0 则相等
+	//	{
+	//		printf("登陆成功\n");
+	//		break;
+	//	}
+	//}
+	//if (i == 3) 
+	//	printf("三次密码均错误,退出程序");
+	//	
+	//
+
+	//int a = 0;
+	//int b = 0; 
+	//int c = 0;
+	//scanf_s("%d%d%d", &a, &b, &c);
+	//if (a < b) {
+	//	int temp = a;
+	//	a = b;
+	//	b = temp;
+	//}
+	//if (a < c) {
+	//	int temp = a;
+	//	a = c;
+	//	c = temp;
+	//}
+
+	//if (b < c) {
+	//	int temp = b;
+	//	b = c;
+	//	c = temp;
+	//}
+	//printf("%d  %d  %d", a,b,c);
+
+
+	//编写程序,数一到100之间九的个数
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0;
+}
